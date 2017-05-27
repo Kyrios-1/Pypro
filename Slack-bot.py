868b8553ce6a07933a125c0c4f87cc5657d814e9
Kyrios-1 <nrvs2010@gmail.com>
2017-05-27 19:55:06 +0800

    slack bot V0.1

---
+++ Slack-bot.py
@@ -0,0 +1,20 @@
+"""Slackbot to post data from ethplorer"""
+import urllib.parse
+import requests
+from slacker import Slacker
+
+#ethplorer constants
+MAIN_API = 'https://api.ethplorer.io/getTokenInfo/0xaec2e87e0a235266d9c5adc9deb4b2e29b54d009?'
+API_KEY = 'freekey'
+#slack constants
+TOKEN = 'xoxb-189756199703-7LsrYwVXdfULLdqYtUyG75Zf'
+SLACK = Slacker(TOKEN)
+
+#grab data from ethplorer
+URL = MAIN_API + urllib.parse.urlencode({'apiKey':API_KEY})
+JSON_DATA = requests.get(URL).json()
+
+#Post on slack
+#print(str(JSON_DATA['symbol'])+" "+"Hodlers"+": "+str(JSON_DATA['holdersCount']))
+
+SLACK.chat.post_message('#general', str(JSON_DATA['symbol'])+" "+"Hodlers"+": "+str(JSON_DATA['holdersCount']))
